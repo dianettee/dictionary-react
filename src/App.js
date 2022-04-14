ff45d5263c377943251e2593a469118f025a94d6
dianettee <dededianeta@gmail.com>
2022-04-14 11:25:10 +0200

    added search form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="App">DICTIONARY by</div>
+        <img src={logo} className="logo img-fluid" alt="logo" />
+        <div className="Dictionary">
+          <Dictionary />
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
