290c5b4d32d9e821794fd70d687b368dada77b65
dianettee <dededianeta@gmail.com>
2022-04-18 20:37:21 +0200

    rendered the word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,7 +8,7 @@ export default function Dictionary() {
   let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    setResults(response.data[0].meanings);
+    setResults(response.data[0]);
   }
 
   function search(event) {
